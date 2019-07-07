3abebaf61118c024904bdeaba2c339b3a3c351d6
Juan Pablo Villaplana <jpvillaplana@gmail.com>
2019-07-07 00:06:04 -0600

    Backpack for Real State Propiedades

---
+++ database/migrations/2019_07_07_060319_create_propiedades_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropiedadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('propiedades', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id');
+            $table->string('nombre')->unique();
+            $table->bigInteger('costo');
+            $table->string('descripcion');
+//            $table->bigInteger('provincia_id')->unsigned()->nullable();
+            $table->string('image')->nullable();
+            $table->string('localizacion')->nullable();
+            $table->string('avaluo')->nullable();
+            $table->string('plano')->nullable();
+            $table->double('tamano_construccion')->nullable();
+            $table->string('tamano_terreno')->nullable();
+            $table->timestamps();
+        });
+
+//        Schema::table('propiedades', function($table) {
+//            $table->foreign('provincia_id')->references('id')->on('provincias');
+//        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('propiedades');
+    }
+}
